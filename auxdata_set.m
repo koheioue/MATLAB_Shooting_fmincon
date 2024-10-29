7c61f20f5762188b2630a711213d4fed865f99d4
koheioue <oue.k.aa@m.titech.ac.jp>
2024-10-29 15:28:05 +0900

    スケーリングした/kmのコメントミスを修正。火星距離を追加

--- auxdata_set.m
+++ auxdata_set.m
@@ -2,6 +2,7 @@
 g0_km_s2         = 9.8e-3;       % 重力定数 (km/s^2)
 M_earth_kg       = 5.972e24;     % 地球の質量 (kg)
 R_sun_earth_km   = 1.496e8;      % 太陽と地球の平均距離 (km)
+R_sun_mars_km    = 227936640; % 火星の公転軌道半径 (km)
 R_earth_km       = 6371;         % 地球の半径（km）
 R_sun_km         = 6.960e5;      % 太陽の半径（km）
 R_mars_km        = 6734;         % 火星の半径（km）
@@ -9,15 +10,16 @@ day_to_sec       = 60*60*24;     % 1日の秒数
 earth_rev_days   = 3.652E+02;    % 地球の公転周期（日）
 
 %% スケーリングファクターを適用
-g0               = g0_km_s2 * tsf^2 / lsf; % 重力定数 (km/s^2)
-R_sun_earth      = R_sun_earth_km / lsf;   % 太陽と地球の平均距離 (km)
-R_earth          = R_earth_km / lsf;       % 地球の半径（km）
-R_sun            = R_sun_km / lsf;         % 太陽の半径（km）
-R_mars           = R_mars_km / lsf;        % 火星の半径（km）
+g0               = g0_km_s2 * tsf^2 / lsf; % 重力定数（スケーリング後）
+R_sun_earth      = R_sun_earth_km / lsf;   % 太陽と地球の距離（スケーリング後）
+R_sun_mars       = R_sun_mars_km  / lsf;   % 太陽と火星の距離（スケーリング後）
+R_earth          = R_earth_km / lsf;       % 地球の半径（スケーリング後）
+R_sun            = R_sun_km / lsf;         % 太陽の半径（スケーリング後）
+R_mars           = R_mars_km / lsf;        % 火星の半径（スケーリング後）
 
 %% 定数の計算（lsf, tsfでスケーリング）
-mu_earth         = 398600.4418 / (lsf^3 / tsf^2); % 地球の重力定数 (km^3/s^2)
-mu_sun           = 1.327124e+11 / (lsf^3 / tsf^2); % 太陽の重力定数 (km^3/s^2)
+mu_earth         = 398600.4418 / (lsf^3 / tsf^2); % 地球の重力定数（スケーリング後）
+mu_sun           = 1.327124e+11 / (lsf^3 / tsf^2); % 太陽の重力定数（スケーリング後）
 mu_se            = mu_earth / (mu_earth + mu_sun); % 地球と太陽の重力定数の比
 omega            = sqrt((mu_sun + mu_earth) / R_sun_earth^3); % 角速度（rad/s）
 
@@ -27,12 +29,15 @@ auxdata.tsf             = tsf;
 auxdata.g0              = g0;
 auxdata.M_earth         = M_earth_kg;
 auxdata.R_sun_earth     = R_sun_earth;
+auxdata.R_sun_mars      = R_sun_mars;
 auxdata.R_sun           = R_sun;
 auxdata.R_earth         = R_earth;
 auxdata.R_mars          = R_mars;
 auxdata.R_earth_km      = R_earth_km;
 auxdata.R_mars_km       = R_mars_km;
 auxdata.R_sun_km        = R_sun_km;
+auxdata.R_sun_earth_km  = R_sun_earth_km;
+auxdata.R_sun_mars_km   = R_sun_mars_km;
 auxdata.mu_earth        = mu_earth;
 auxdata.mu_sun          = mu_sun;
 auxdata.mu_se           = mu_se;
